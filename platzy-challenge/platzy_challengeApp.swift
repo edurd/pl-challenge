f1ab59c21ce9e928526065a9124cacc725089bc1
Eduardo Hoyos <edu.hoyos92@gmail.com>
2023-07-14 16:44:19 -0500

    Initial Commit

---
+++ platzy-challenge/platzy_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  platzy_challengeApp.swift
+//  platzy-challenge
+//
+//  Created by Eduardo Hoyos Loli on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct platzy_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
